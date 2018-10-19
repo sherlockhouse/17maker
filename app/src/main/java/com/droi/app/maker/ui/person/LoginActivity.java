be5feeb49c212d92b7dfe249fec55a709acfb34a
guikai <guikai@droi.com>
2018-10-19 19:17:37 +0800

    17maker: ui login fragment
    Change-Id: Icf3e356062b5f157162656f0d171bfcac48477dd

---
+++ app/src/main/java/com/droi/app/maker/ui/person/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.droi.app.maker.ui.person;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.droi.app.maker.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
